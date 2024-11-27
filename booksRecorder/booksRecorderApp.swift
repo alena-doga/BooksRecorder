ab9201970f34aad0529b36f8ac8f9cf977cc37b8
Alena Malinovska <alenadoga8@gmail.com>
2024-11-27 17:46:36 +0200

    Initial Commit

---
+++ booksRecorder/booksRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  booksRecorderApp.swift
+//  booksRecorder
+//
+//  Created by Alena on 27.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct booksRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
